c8e8938920fb23ca4029d1c216a6a1c72dde9437
Md. Al-Amin <37890258+AlAmincseru@users.noreply.github.com>
2019-11-25 23:36:39 -0800

    Add files via upload

---
+++ MAL/String_reverse.asm
@@ -0,0 +1,33 @@
+; string 
+.model small
+.code
+main proc
+    mov cx,0
+    
+    pushin:
+        mov ah,1
+        int 21h
+        mov bl,al
+        
+        cmp bl,0dh
+        je newline
+        push bx
+        inc cx
+        jmp pushin
+        
+     newline:
+        mov ah,2
+        mov dl,0dh
+        mov dl,0ah
+        int 21h 
+        
+     popout:
+        pop dx
+        int 21h
+        loop popout
+        
+        main endp
+end main
+        
+        
+
